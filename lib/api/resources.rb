3101224515c0ff0955925cc595bfe34e3bbdb996
Paulina Levit <levit@nyu.edu>
2017-02-20 22:38:23 -0500

    score create with tests [Delivers #139436521]

--- lib/api/resources.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-module API
-  module Resources
-    extend ActiveSupport::Autoload
-  end
-end
---
+++ spec/requests/score_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+describe 'Score CRUD', type: :request do 
+	let(:user) { FactoryGirl.create(:user) }
+	let(:experiment) { FactoryGirl.create(:experiment) }
+	let(:sample) { FactoryGirl.create(:sample) }
+
+	context 'PUT /score' do 
+		before do
+			put '/v3/score', 
+				user_id: user.id,
+				experiment_id: experiment.id,
+				sample_id: sample.id,
+				rating: 0.5
+		end
+
+		let(:result) { JSON.parse(response.body) }
+
+		it 'should create a score' do 
+			binding.pry
+			expect(response.code).to eql('201')
+		end
+
+		it 'should set the proper ids' do
+			expect(result["user"]["links"].first["href"]).to eql(
+				"http://www.example.com/v3/user/#{user.id}"
+			)
+			expect(result["experiment"]["links"].first["href"]).to eql(
+				"http://www.example.com/v3/experiment/#{experiment.id}"
+			)
+			expect(result["sample"]["links"].first["href"]).to eql(
+				"http://www.example.com/v3/sample/#{sample.id}"
+			)
+		end
+
+		context 'with invalid/missing parameters' do 
+			before do
+				put '/v3/score', 
+					user_id: user.id,
+					experiment_id: experiment.id,
+					sample_id: sample.id
+			end
+			it 'should enforce required' do 
+				expect(response.code).to eql('400')
+				expect(JSON.parse(response.body)).to have_key('error')
+			end
+		end
+	end
+end
