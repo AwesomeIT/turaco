d236da1bb5dea774dd929f251f010e73f14a9d3e
Paulina Levit <levit@nyu.edu>
2017-03-26 19:51:36 -0400

    removed catch all
    [Delivers #139436533]

--- lib/api/root.rb
+++ lib/api/root.rb
@@ -3,12 +3,7 @@ module API
   class Root < Grape::API
     format :json
     formatter :json, Grape::Formatter::Roar
-
-    # TODO: don't be lazy when handling errors
-    rescue_from :all do |err|
-      error!(err, 500)
-    end
-
+    
     mount Endpoints::Score => '/score'
     mount Endpoints::Sample => '/sample'
     mount Endpoints::Experiment => '/experiment'
