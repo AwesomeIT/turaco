e31b16d7e03c1ff870095780ad97151d4ea85eda
David Stancu <dstancu@nyu.edu>
2017-04-09 20:30:31 -0400

    lint

--- app/controllers/doorkeeper/custom_authorizations_controller.rb
+++ app/controllers/doorkeeper/custom_authorizations_controller.rb
@@ -1,12 +1,12 @@
+# frozen_string_literal: true
 module Doorkeeper
   class CustomAuthorizationsController < Doorkeeper::AuthorizationsController
-
-    # 
-    # We associate scopes with permissions levels stored in 
-    # our database. This is a custom subclass of 
-    # Doorkeeper::OAuth::PreAuthentication with logic to ensure that 
+    #
+    # We associate scopes with permissions levels stored in
+    # our database. This is a custom subclass of
+    # Doorkeeper::OAuth::PreAuthentication with logic to ensure that
     # users cannot violate their role levels.
-    # 
+    #
     # @return Extensions::Doorkeeper::ScopedPreAuth
     def pre_auth
       @pre_auth ||= begin
