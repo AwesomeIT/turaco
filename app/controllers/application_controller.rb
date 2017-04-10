f7f98a724fa4b58259ed01d7cc32280d30ccdd65
David Stancu <dstancu@nyu.edu>
2017-04-09 20:18:14 -0400

    param style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   after_filter :store_location
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     session[:previous_url] || root_path
   end
 end
