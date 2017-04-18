9a2512e08b8d7b27a04b46c25134bb0bf9b19208
David Stancu <mach@kemosabe.hubble.lan>
2017-04-17 21:32:38 -0400

    add codeclimate and simplecov
    [#143384803]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
+# Test coverage
+require "simplecov"
+SimpleCov.start
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
