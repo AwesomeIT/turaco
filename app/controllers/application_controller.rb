31775276bb2091f366c8aeacd29dae65c4a76867
David Stancu <dstancu@nyu.edu>
2017-04-29 17:54:20 -0400

    redirect after sign out too

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for(_resource)
+  def after_login_action(_resource)
     session[:previous_url] || root_path
   end
+
+  # rubocop:disable Style/Alias
+  alias_method :after_sign_out_path_for, :after_login_action
+  alias_method :after_sign_in_path_for, :after_login_action
+  # rubocop:enable Style/Alias
 end
