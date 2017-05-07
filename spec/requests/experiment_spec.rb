7844c444dc186cec602068627fc15134c8d23793
David Stancu <dstancu@nyu.edu>
2017-05-07 13:16:39 -0400

    es does this query now / redundant test
    [#144252707]

--- spec/requests/experiment_spec.rb
+++ spec/requests/experiment_spec.rb
@@ -124,22 +124,5 @@ describe 'Experiment CRUD', type: :request do
         expect(results['experiments'].count).to eql(5)
       end
     end
-
-    context 'with querystring filters' do 
-      before do
-        experiments.last.name = 'foobar'
-        experiments.last.save
-
-        get '/v3/experiments', params: { name: 'foobar' }, headers: { 
-          'Authorization' => "Bearer #{token.token}" 
-        }
-      end
-
-      it 'should return the correct experiments' do 
-        expect(response.code).to eql('200')
-        expect(results['experiments'].first['links'].first['href'])
-        .to eql("http://www.example.com/v3/experiment/#{experiments.last.id}")
-      end
-    end
   end
 end
