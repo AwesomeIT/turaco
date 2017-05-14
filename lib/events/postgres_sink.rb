e51db635cde2bc5e79e827b65b243537791e6c05
David Stancu <dstancu@nyu.edu>
2017-05-13 21:34:52 -0400

    add sample_destroyed to postgres event sink
    [#145323091]
    
    [#145004659]

--- lib/events/postgres_sink.rb
+++ lib/events/postgres_sink.rb
@@ -39,5 +39,10 @@ module Events
       respond_to :sample_speech_recognition,
                  message: model_base_message(model)
     end
+
+    def sample_destroyed(model)
+      respond_to :sample_delete_from_s3,
+                 message: model_base_message(model).merge(s3_key: model.s3_key)
+    end
   end
 end
