28d60413066407e0b894a9a56af1efe3fb252f51
David Stancu <dstancu@nyu.edu>
2017-05-20 11:53:11 -0400

    lint + gemfile

--- lib/permissions/participant.rb
+++ lib/permissions/participant.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module Permissions
   class Participant < Warhol::Ability
     define_permissions do
--- lib/permissions/researcher.rb
+++ lib/permissions/researcher.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module Permissions
   class Researcher < Warhol::Ability
     define_permissions do
